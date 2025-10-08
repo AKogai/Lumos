54d4e9c66bb91ad68d100392e28761b229aef576
Edgars Tubelis <edgars.tubelis@rapid-data.de>
2025-10-08 22:07:32 +0200

    default value reset fix

--- frontend/src/components/condolence-select/condolence-select.tsx
+++ frontend/src/components/condolence-select/condolence-select.tsx
@@ -1,8 +1,21 @@
-import { Box, Stack, Typography } from '@mui/material';
-import { useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { useMemo, useState } from 'react';
 
 export const CondolenceSelect = ({ suggestions }: { suggestions: Array<string> }) => {
   const [selectedIdx, setSelectedIdx] = useState<number | null>(null);
+  const [isSettingValue, setIsSettingValue] = useState(false);
+
+  const currentDefaultValue = useMemo(() => suggestions[selectedIdx] || '', [selectedIdx, suggestions]);
+
+  const updateSelectedIdx = (index: number) => {
+    setIsSettingValue(true);
+    setSelectedIdx(index === selectedIdx ? null : index);
+    setTimeout(() => setIsSettingValue(false), 100);
+  };
+
+  const handleSaveCondolence = () => {
+    console.log('TODO');
+  };
 
   return (
     <Stack spacing={2} direction="column">
@@ -10,11 +23,23 @@ export const CondolenceSelect = ({ suggestions }: { suggestions: Array<string> }
         <Box
           key={index}
           sx={{ padding: 2, border: index === selectedIdx ? '1px solid blue' : '1px solid #ccc', borderRadius: 1 }}
-          onClick={() => setSelectedIdx(index)}
+          onClick={() => updateSelectedIdx(index)}
         >
           <Typography>{s}</Typography>
         </Box>
       ))}
+      {selectedIdx !== null && (
+        <>
+          {!isSettingValue ? (
+            <TextField multiline rows={4} defaultValue={currentDefaultValue} />
+          ) : (
+            <TextField multiline rows={4} disabled />
+          )}
+          <Button onClick={handleSaveCondolence} variant="contained">
+            Save
+          </Button>
+        </>
+      )}
     </Stack>
   );
 };
