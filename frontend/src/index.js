2da5303d0da5ed303780607b779d07b858d98c9b
Finn Hansen <finn.hansen@rapid-data.de>
2025-10-08 18:47:39 +0200

    added font

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,22 +5,25 @@ import App from './App';
 import { ReactQueryProvider } from './provider/react-query';
 import { closeSnackbar, SnackbarProvider } from 'notistack';
 import { Button } from '@mui/material';
+import { MuiProvider } from './provider/mui';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ReactQueryProvider>
-      <SnackbarProvider
-        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-        preventDuplicate={true}
-        action={(snackbarKey) => (
-          <Button variant="text" sx={{ color: 'white' }} onClick={() => closeSnackbar(snackbarKey)}>
-            Close
-          </Button>
-        )}
-      >
-        <App />
-      </SnackbarProvider>
-    </ReactQueryProvider>
+    <MuiProvider>
+      <ReactQueryProvider>
+        <SnackbarProvider
+          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+          preventDuplicate={true}
+          action={(snackbarKey) => (
+            <Button variant="text" sx={{ color: 'white' }} onClick={() => closeSnackbar(snackbarKey)}>
+              Close
+            </Button>
+          )}
+        >
+          <App />
+        </SnackbarProvider>
+      </ReactQueryProvider>
+    </MuiProvider>
   </React.StrictMode>
 );
