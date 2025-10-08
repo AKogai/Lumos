f8999beac075446ae34806ca27c4f1b5b9f0ec92
Edgars Tubelis <edgars.tubelis@rapid-data.de>
2025-10-08 11:37:42 +0200

    reset btn

--- frontend/src/components/stepper/stepper.tsx
+++ frontend/src/components/stepper/stepper.tsx
@@ -37,47 +37,61 @@ export const Stepper = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
   return (
     <Box sx={{ width: "100%" }}>
-      <MuiStepper activeStep={activeStep} orientation="vertical">
-        {steps.map((step, index) => {
-          const stepProps: { completed?: boolean } = {};
-          const labelProps: {
-            optional?: React.ReactNode;
-          } = {};
-          if (step.optional) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
+      <Paper square elevation={0} sx={{ p: 3 }}>
+        <MuiStepper activeStep={activeStep} orientation="vertical">
+          {steps.map((step, index) => {
+            const stepProps: { completed?: boolean } = {};
+            const labelProps: {
+              optional?: React.ReactNode;
+            } = {};
+            if (step.optional) {
+              labelProps.optional = (
+                <Typography variant="caption">Optional</Typography>
+              );
+            }
+            if (isStepSkipped(index)) {
+              stepProps.completed = false;
+            }
+            return (
+              <Step key={index} {...stepProps}>
+                <StepLabel {...labelProps}>{step.label}</StepLabel>
+                <StepContent>
+                  <Box sx={{ mb: 2 }}>
+                    <Button
+                      variant="contained"
+                      onClick={handleNext}
+                      sx={{ mt: 1, mr: 1 }}
+                    >
+                      {index === steps.length - 1 ? "Finish" : "Continue"}
+                    </Button>
+                    <Button
+                      disabled={index === 0}
+                      onClick={handleBack}
+                      sx={{ mt: 1, mr: 1 }}
+                    >
+                      Back
+                    </Button>
+                  </Box>
+                </StepContent>
+              </Step>
             );
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={index} {...stepProps}>
-              <StepLabel {...labelProps}>{step.label}</StepLabel>
-              <StepContent>
-                <Box sx={{ mb: 2 }}>
-                  <Button
-                    variant="contained"
-                    onClick={handleNext}
-                    sx={{ mt: 1, mr: 1 }}
-                  >
-                    {index === steps.length - 1 ? "Finish" : "Continue"}
-                  </Button>
-                  <Button
-                    disabled={index === 0}
-                    onClick={handleBack}
-                    sx={{ mt: 1, mr: 1 }}
-                  >
-                    Back
-                  </Button>
-                </Box>
-              </StepContent>
-            </Step>
-          );
-        })}
-      </MuiStepper>
+          })}
+        </MuiStepper>
+      </Paper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Reset
+          </Button>
+        </Paper>
+      )}
     </Box>
   );
 };
